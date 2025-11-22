4355f9d2c2b518869d2daf563b119356d29dd80c
Tuan Dat Pham <88708244+tuandatdavid@users.noreply.github.com>
2025-11-22 12:29:44 +0100

    Update ChangeBlur.sh

--- config/hypr/scripts/ChangeBlur.sh
+++ config/hypr/scripts/ChangeBlur.sh
@@ -7,11 +7,11 @@ notif="$HOME/.config/swaync/images"
 STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
 
 if [ "${STATE}" == "2" ]; then
-	hyprctl keyword decoration:blur:size 2
+	hyprctl keyword decoration:blur:size 4
 	hyprctl keyword decoration:blur:passes 1
- 	notify-send -e -u low -i "$notif/note.png" " Less Blur"
+ 	notify-send -e -u low -i "$notif/note.png" " Normal Blur"
 else
 	hyprctl keyword decoration:blur:size 5
 	hyprctl keyword decoration:blur:passes 2
-  	notify-send -e -u low -i "$notif/ja.png" " Normal Blur"
+  	notify-send -e -u low -i "$notif/ja.png" " More Blur"
 fi
